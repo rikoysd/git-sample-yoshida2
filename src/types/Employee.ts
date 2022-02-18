7b7bcca9da4bd3e3355fe0bf6e5e8e9adc74b0ed
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-02-18 15:45:16 +0900

    フィールド変数名の変更

--- src/types/Employee.ts
+++ src/types/Employee.ts
@@ -1,25 +1,19 @@
-export class Employee{
-  constructor(
-    private _id:number,
-    private _name:string    
-  ){}
+export class Employee {
+  constructor(private _id: number, private _address: string) {}
 
-    public get id(): number
- {
-        return this._id;
-    }
+  public get id(): number {
+    return this._id;
+  }
 
-    public set id(id: number
-) {
-        this._id = id;
-    }
+  public set id(id: number) {
+    this._id = id;
+  }
 
-    public get name(): string {
-        return this._name;
-    }
+  public get name(): string {
+    return this._name;
+  }
 
-    public set name(name: string) {
-        this._name = name;
-    }
-
+  public set name(name: string) {
+    this._name = name;
+  }
 }
