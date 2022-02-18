01c2c949a823409f0ee8aa7597c1a423ff931457
rikoysd <riko.yoshida@rakus-partners.co.jp>
2022-02-18 15:20:32 +0900

    従業員クラス作成

---
+++ src/types/Employee.ts
@@ -0,0 +1,25 @@
+export class Employee{
+  constructor(
+    private _id:number,
+    private _name:string    
+  ){}
+
+    public get id(): number
+ {
+        return this._id;
+    }
+
+    public set id(id: number
+) {
+        this._id = id;
+    }
+
+    public get name(): string {
+        return this._name;
+    }
+
+    public set name(name: string) {
+        this._name = name;
+    }
+
+}
